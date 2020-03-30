a8ad71a3ae5427eb9682cc0dad9945796333615a
Chandru <chandru89new@gmail.com>
2020-03-30 11:48:23 +0530

    data update

--- run.js
+++ run.js
@@ -1,13 +1,37 @@
 const terminal = require("child_process").execSync;
-const commands = [
+const COMMANDS_METRICS = [
   "cd origin && git pull origin master",
   "cd data && node compute",
-  "git commit data",
+  "git add data",
   'git commit -m "data update"',
   "git push origin master"
 ];
+const COMMANDS_WEBSITE = [
+  "cd web && parcel build index.html",
+  "cd web && firebase deploy"
+];
 
 const newLine = () => console.log("\n");
+
+// type runType = "data" | "website"
+
+const main = () => {
+  const runType = process.argv[2];
+  run(runType);
+};
+
+const run = runType => {
+  switch (runType) {
+    case "data":
+      runCommands(COMMANDS_METRICS)();
+      break;
+    case "website":
+      runCommands(COMMANDS_WEBSITE);
+      break;
+    default:
+      return console.log("No run type given.");
+  }
+};
 
 const runCommands = list => {
   while (list.length) {
@@ -28,4 +52,4 @@ const runCommands = list => {
   console.log(`--------- DONE ------------`);
 };
 
-runCommands(commands);
+main(process.argv[2]);
